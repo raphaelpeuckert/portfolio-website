20cce3a6644dcbf1983e5286256f6fc6b0a0f471
Raphael Peuckert <raphael@spdigital.agency>
2023-04-05 14:10:38 +0200

    quicksave

--- lang/homeLanguage.js
+++ lang/homeLanguage.js
@@ -1,4 +1,7 @@
 const homePage = document.querySelector('#home-page');
+let homeAbout = document.querySelector('.home--about');
+
+console.log(document.querySelector('.home--about').childNodes.h2)
 
 let language = navigator.language.split('-').slice(0,1).toString();
 
@@ -7,6 +10,7 @@ window.onload = () => {
     if(language === 'de') {
         document.title = 'Start | Raphael Peuckert';
         document.querySelector('.home-subtitle').innerHTML = 'FRONTEND-ENTWICKLER';
+        document.querySelector('.home--about') = 'test';
     }
 
 };
